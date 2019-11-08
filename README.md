eed93375084512eb3708972bf44cdd2618444bc2
august.moller <ag.man@live.se>
2019-11-08 14:33:03 +0100

    added touch on canvas screen

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-"# Pumpkin-Snake" 
+# Pumpkin Snake
 
 This is snake but with a pumpkin using javascript.
 
-Play it here: https://agman10.github.io/Pumpkin-Snake/
+### Todo:
+
+- Touch is a little bugged, while holding you can easly suicide at a state you shouldn't be able to on desktop version.
+
+- Add a title screen and a proper game over screen.
+
+- Move the score to a better place and make the font less blurry.
+
+- Add selectable skins skins to the player.
+
+- Fix so the last tail also dissapears when you die.
+
+- Fix better mobile support.
+
+- add sound?
+
+Play it here: https://snake.agman.xyz/
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 canvas.setAttribute("id", "canvas");
 
 //website don't scroll when using arrow keys
-window.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function (e) {
     // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
@@ -24,12 +24,46 @@ var candy;
  * detects key presses
  */
 
+
+//touch controls
+canvas.addEventListener('touchstart', event => {
+    lastTouch = event;
+
+});
+
+var lastTouch
+canvas.addEventListener('touchmove', event => {
+    event.preventDefault()
+    if (!player.alive) return
+    var deltaX = lastTouch.changedTouches[0].clientX - event.changedTouches[0].clientX
+    var deltaY = lastTouch.changedTouches[0].clientY - event.changedTouches[0].clientY
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        if (deltaX > 0 && player.direction != "right") {
+            player.direction = "left";
+        } else if (player.direction != "left") {
+            player.direction = "right";
+        }
+    }
+    if (Math.abs(deltaY) > Math.abs(deltaX)) {
+        if (deltaY > 0 && player.direction != "down") {
+            player.direction = "up";
+        } else if (player.direction != "up") {
+            player.direction = "down";
+        }
+    }
+
+    //console.log("x" + event.changedTouches[0].clientX);
+    //console.log("y" + event.changedTouches[0].clientY);
+    lastTouch = event;
+
+});
+
 var keysDown = [];
 
 /**
  * when key is not pressed, used to check if key is
  * pressed so you can't go back too quickly
-*/
+ */
 var keypressed = false;
 
 document.addEventListener("keydown", event => {
@@ -37,34 +71,13 @@ document.addEventListener("keydown", event => {
 
     keysDown[event.keyCode] = true;
     keypressed = true;
-    
+
 
 })
 
 //gives the html buttons keypresses
-function upButton(){
-    if (player.direction != "down" && player.direction != "up" && player.alive) {
-        player.direction = "up";
-    }
-}
-function leftButton(){
-    if (player.direction != "right" && player.direction != "left" && player.alive) {
-        player.direction = "left";
-
-    }
-}
-function downButton(){
-    if (player.direction != "up" && player.direction != "down" && player.alive) {
-        player.direction = "down";
-    }
-}
-function rightButton(){
-    if (player.direction != "left" && player.direction != "right" && player.alive){
-        player.direction = "right";
-    }
-}
-function spaceButton(){
-    if(!player.alive){
+function spaceButton() {
+    if (!player.alive) {
         game.reset();
     }
 }
@@ -80,8 +93,8 @@ window.onload = () => {
     gridX = player.sprite.width
     gridY = player.sprite.height
     candy = new Candy();
-    
-    
+
+
     game.start();
 }
 
